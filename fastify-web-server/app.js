0149eb43936808a8c13d1bc6d067a693daf8f12f
Laszlo Balint <laszlobalint1987@gmail.com>
2022-04-18 13:56:11 +0200

    dynamic view loading

--- fastify-web-server/app.js
+++ fastify-web-server/app.js
@@ -3,7 +3,16 @@
 const path = require("path");
 const AutoLoad = require("fastify-autoload");
 
+const pointOfView = require("point-of-view");
+const handlebars = require("handlebars");
+
 module.exports = async function (fastify, opts) {
+  fastify.register(pointOfView, {
+    engine: { handlebars },
+    root: path.join(__dirname, "views"),
+    layout: "layout.hbs",
+  });
+
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, "plugins"),
     options: Object.assign({}, opts),
@@ -12,5 +21,12 @@ module.exports = async function (fastify, opts) {
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, "routes"),
     options: Object.assign({}, opts),
+  });
+  fastify.setNotFoundHandler((request, reply) => {
+    if (request.method !== "GET") {
+      reply.status(405);
+      return "Method Not Allowed\n";
+    }
+    return "Not Found\n";
   });
 };
--- fastify-web-server/routes/root.js
+++ fastify-web-server/routes/root.js
@@ -1,7 +1,5 @@
-'use strict'
-
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+module.exports = async (fastify, opts) => {
+  fastify.get("/", async (request, reply) => {
+    return reply.view("index.hbs");
+  });
+};
