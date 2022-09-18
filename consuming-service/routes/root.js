b2b8d43125fe6c14b1b2bff50f8570a915a19f6b
László, Bálint <balint.laszlo@lhsystems.com>
2022-09-18 18:23:32 +0200

    rest stuff

---
+++ consuming-service/routes/root.js
@@ -0,0 +1,40 @@
+Managing Status Codes (3)
+If the ID is not an integer, the upstream services respond with a 400 status code (Bad Request). Let's handle these the same way we handle the 404s. We can modify routes/root.js to the following:
+
+'use strict'
+
+const got = require('got')
+
+const {
+  BICYCLE_SERVICE_PORT = 4000, BRAND_SERVICE_PORT = 5000
+} = process.env
+
+const bicycleSrv = `http://localhost:${BICYCLE_SERVICE_PORT}`
+const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`
+
+module.exports = async function (fastify, opts) {
+  const { httpErrors } = fastify
+  fastify.get('/:id', async function (request, reply) {
+    const { id } = request.params
+    try {
+      const [ bicycle, brand ] = await Promise.all([
+        got(`${bicycleSrv}/${id}`).json(),
+        got(`${brandSrv}/${id}`).json()
+      ])
+      return {
+        id: bicycle.id,
+        color: bicycle.color,
+        brand: brand.name,
+      }
+    } catch (err) {
+      if (!err.response) throw err
+      if (err.response.statusCode === 404) {
+        throw httpErrors.notFound()
+      }
+      if (err.response.statusCode === 400) {
+        throw httpErrors.badRequest()
+      }
+      throw err
+    }
+  })
+}
